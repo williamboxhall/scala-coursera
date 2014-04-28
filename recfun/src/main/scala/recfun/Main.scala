c8c43d200756853a27d220c92c326cfed7f1a492
William Boxhall <williamboxhall@gmail.com>
2014-04-28 14:54:01 -0700

    messy first implementation of balance

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -20,7 +20,28 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balance(numClosesNeeded: Int, chars: List[Char]): Boolean = {
+      if (chars.isEmpty) {
+        return numClosesNeeded == 0
+      }
+
+      val head = chars.head
+
+      if (head == '(') {
+        balance(numClosesNeeded + 1, chars.tail)
+      } else if (head == ')') {
+        if (numClosesNeeded == 0) {
+          false
+        } else {
+          balance(numClosesNeeded - 1, chars.tail)
+        }
+      } else {
+        balance(numClosesNeeded, chars.tail)
+      }
+    }
+    balance(0, chars)
+  }
 
   /**
    * Exercise 3
--- recfun/src/test/scala/recfun/BalanceSuite.scala
+++ recfun/src/test/scala/recfun/BalanceSuite.scala
@@ -9,6 +9,10 @@ import org.scalatest.junit.JUnitRunner
 class BalanceSuite extends FunSuite {
   import Main.balance
 
+  test("balance: empty string is balanced") {
+    assert(balance(Nil))
+  }
+
   test("balance: '(if (zero? x) max (/ 1 x))' is balanced") {
     assert(balance("(if (zero? x) max (/ 1 x))".toList))
   }
