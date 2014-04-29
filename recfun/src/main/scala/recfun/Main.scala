44cdd12606840a106383a390d981cfbdf7e923bb
William Boxhall <williamboxhall@gmail.com>
2014-04-29 16:33:30 -0700

    removed useless comments

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -10,16 +10,10 @@ object Main {
     }
   }
 
-  /**
-   * Exercise 1
-   */
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
-  /**
-   * Exercise 2
-   */
   def balance(chars: List[Char]): Boolean = {
     def deltaFor(char: Char): Int = {
       char match {
@@ -36,9 +30,6 @@ object Main {
     balance(0, chars)
   }
 
-  /**
-   * Exercise 3
-   */
   def countChange(money: Int, coins: List[Int]): Int = {
     def countChange(runningTotal: Int, money: Int, coins: List[Int]): Int = {
       if (runningTotal > money || coins.isEmpty || coins.head > money) 0
