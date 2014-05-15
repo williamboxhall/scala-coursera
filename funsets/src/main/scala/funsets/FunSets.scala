7d65c40971dc0ac19a29153eb584af59591724f4
William Boxhall <williamboxhall@gmail.com>
2014-05-15 11:55:03 +0200

    fix singleton operands & and | to be && and || so they can perform early returns

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -7,13 +7,13 @@ object FunSets {
 
   def singletonSet(elem: Int): Set = _ == elem
 
-  def union(s: Set, t: Set): Set = x => s(x) | t(x)
+  def union(s: Set, t: Set): Set = x => s(x) || t(x)
 
-  def intersect(s: Set, t: Set): Set = x => s(x) & t(x)
+  def intersect(s: Set, t: Set): Set = x => s(x) && t(x)
 
-  def diff(s: Set, t: Set): Set = x => s(x) & !t(x)
+  def diff(s: Set, t: Set): Set = x => s(x) && !t(x)
 
-  def filter(s: Set, p: Int => Boolean): Set = x => s(x) & p(x)
+  def filter(s: Set, p: Int => Boolean): Set = x => s(x) && p(x)
 
   def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, !p(_))
 
@@ -24,7 +24,7 @@ object FunSets {
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
       if (a > bound) true
-      else if (s(a) & !p(a)) false
+      else if (s(a) && !p(a)) false
       else iter(a + 1)
     }
     iter(-bound)
