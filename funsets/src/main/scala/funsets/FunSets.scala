99262187ba222515476f2d8d7830b3df27159910
William Boxhall <williamboxhall@gmail.com>
2014-05-15 11:52:07 +0200

    autoformat

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -2,15 +2,25 @@ package funsets
 
 object FunSets {
   type Set = Int => Boolean
+
   def contains(s: Set, elem: Int): Boolean = s(elem)
+
   def singletonSet(elem: Int): Set = _ == elem
+
   def union(s: Set, t: Set): Set = x => s(x) | t(x)
+
   def intersect(s: Set, t: Set): Set = x => s(x) & t(x)
+
   def diff(s: Set, t: Set): Set = x => s(x) & !t(x)
+
   def filter(s: Set, p: Int => Boolean): Set = x => s(x) & p(x)
+
   def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, !p(_))
+
   def map(s: Set, f: Int => Int): Set = (x: Int) => exists(s, f(_) == x)
+
   val bound = 1000
+
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
       if (a > bound) true
@@ -18,7 +28,8 @@ object FunSets {
       else iter(a + 1)
     }
     iter(-bound)
-    }
+  }
+
   def toString(s: Set): String = {
     val xs = for (i <- -bound to bound if contains(s, i)) yield i
     xs.mkString("{", ",", "}")
