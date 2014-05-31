3f9a2febdf66b12c40109fa986a1b2d3d7220fa0
William Boxhall <williamboxhall@gmail.com>
2014-05-31 16:59:43 +0200

    implement singleton

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -102,7 +102,7 @@ object Huffman {
   /**
    * Checks whether the list `trees` contains only one single code tree.
    */
-  def singleton(trees: List[CodeTree]): Boolean = ???
+  def singleton(trees: List[CodeTree]): Boolean = trees.length == 1
 
   /**
    * The parameter `trees` of this function is a list of code trees ordered
--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -41,6 +41,16 @@ class HuffmanSuite extends FunSuite {
     assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 3)))
   }
 
+  test("singleton") {
+    new TestTrees {
+      assert(singleton(List(Leaf('a', 1))))
+      assert(singleton(List(t1)))
+      assert(singleton(List(t2)))
+      assert(!singleton(List(Leaf('a', 1), Leaf('v', 1))))
+      assert(!singleton(List()))
+    }
+  }
+
   test("combine of some leaf list") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
     assert(combine(leaflist) === List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)))
