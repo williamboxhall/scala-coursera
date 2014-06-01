1529bfbfef55a89edc1f9fa9da2483ce507859bf
William Boxhall <williamboxhall@gmail.com>
2014-06-01 09:47:14 +0200

    complete quickEncode

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -238,14 +238,19 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = convertAcc(tree, List())
+
+  def convertAcc(tree: CodeTree, acc: List[Bit]): CodeTable = tree match {
+    case Leaf(char, _) => List((char, acc))
+    case Fork(left, right, _, _) => mergeCodeTables(convertAcc(left, acc :+ 0), convertAcc(right, acc :+ 1))
+  }
 
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = a ::: b
 
   /**
    * This function encodes `text` according to the code tree `tree`.
